1206696dca3586aba3d18e5e3d53576e563f4cf9
Nomar Olivas <nomarolivas@gmail.com>
2025-06-24 16:29:06 -0700

    Added Navbar page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="bg-fuchsia-500 h-[4000px}">Navbardddd</div>
+
+
+        </div>
+        
+    )
+}
+
+export default Navbar;
